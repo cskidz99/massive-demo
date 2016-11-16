92c3f328b56643fa7e7e91e33f9f887a20f78804
Cameron Skidmore <cskidz99@gmail.com>
2016-11-16 14:06:08 -0700

    Completed through step 12

---
+++ db/get_all_incidents.sql
@@ -0,0 +1,12 @@
+select inc.id,
+  inc.us_state,
+  inj.name as injury,
+  a.name as affected_area,
+  c.name as cause
+from incidents inc
+join injuries inj
+  on inc.injury_id = inj.id
+join affected_areas a
+  on a.id = inj.affected_area_id
+join causes c
+  on c.id = inc.cause_id
